48b99dc7f6ba76516277493d8695be1638c4fbc7
AbelJoy7 <157111865+AbelJoy7@users.noreply.github.com>
2026-04-15 13:04:00 +0530

    Fixed models import issue

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI,Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-import models
+from backend import models
 from fastapi import Depends
 from sqlalchemy.orm import Session
 from backend.database import engine, Base, SessionLocal
