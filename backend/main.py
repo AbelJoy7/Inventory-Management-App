ce28eab413daae89b1d3c4dec3609604a14b7b98
AbelJoy7 <157111865+AbelJoy7@users.noreply.github.com>
2026-04-15 14:33:40 +0530

    Test minimal app

--- backend/main.py
+++ backend/main.py
@@ -4,10 +4,23 @@ from backend import models
 from fastapi import FastAPI
 from backend.database import engine
 from backend import models
+from fastapi.middleware.cors import CORSMiddleware
 
 print("✅ MODELS LOADED")
 
 app = FastAPI()
+origins = [
+    "http://localhost:3000",
+    "https://inventory-management-app-5lj8-ancjpxccl.vercel.app",  # replace this
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
